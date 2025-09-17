4cae091d7d4f1caef1f688258d4b1c8fd399a2a5
thevery-nvg <129183313+thevery-nvg@users.noreply.github.com>
2025-09-17 18:01:47 +0400

    добавлен слой добавления канала в роутер. чат для комментариев

--- src/handlers/manage_channels/add_channel.py
+++ src/handlers/manage_channels/add_channel.py
@@ -2,14 +2,12 @@ from datetime import datetime
 
 from aiogram import Router, F, types
 from aiogram.fsm.context import FSMContext
-from aiogram.utils.keyboard import InlineKeyboardBuilder
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.core.crud import add_channel
 from src.handlers.mock import Channel
 from src.handlers.utils import (
     Buttons,
-    goto_main_menu_btn,
     Admin,
     get_channel_details_text,
     yes_no_keyboard,
@@ -17,6 +15,29 @@ from src.handlers.utils import (
 )
 
 router = Router(name="add_channel")
+
+
+async def add_new_channel(data, db_session):
+    name = data.get("channel_name")
+    channel_id = data.get("channel_id")
+    notification_id = data.get("notification_id")
+    moderation_enabled = data.get("moderation_enabled")
+    if moderation_enabled:
+        comment_chat_id = data.get("comment_chat_id")
+    else:
+        comment_chat_id = None
+    new_channel = Channel(
+        id=channel_id,
+        name=name,
+        is_active=True,
+        moderation_enabled=moderation_enabled,
+        comment_chat_id=comment_chat_id,
+        notification_chat_id=notification_id if notification_id else None,
+        created_at=datetime.now(),
+        updated_at=datetime.now(),
+    )
+    channel = await add_channel(db_session, new_channel)
+    return channel
 
 
 @router.callback_query(F.data == Buttons.add_channel_callback, Admin.manage_channels)
@@ -86,36 +107,66 @@ async def add_channel_stage_4(message: types.Message, state: FSMContext):
     await state.update_data(notification_id=notification_id)
     await state.set_state(Admin.change_moderation)
     await main_message.message.edit_text(
-        text="Нужно ли модерировать канал?", reply_markup=yes_no_keyboard()
+        text="Нужно ли модерировать комментарии канала?", reply_markup=yes_no_keyboard()
     )
 
 
-@router.message(Admin.change_moderation)
-async def add_channel_stage_5(
+@router.callback_query(Admin.change_moderation, F.data == Buttons.yes_sure_callback)
+async def add_channel_stage_5_yes(
     callback_query: types.CallbackQuery, state: FSMContext, db_session: AsyncSession
 ):
     data = await state.get_data()
     main_message = data.get("main_message")
-    name = data.get("channel_name")
-    channel_id = data.get("channel_id")
-    notification_id = data.get("notification_id")
-
-    if callback_query.data == Buttons.yes_sure_callback:
-        moderation_enabled = True
-    else:
-        moderation_enabled = False
-
-    new_channel = Channel(
-        id=channel_id,
-        name=name,
-        is_active=True,
-        moderation_enabled=moderation_enabled,
-        notification_chat_id=notification_id if notification_id else None,
-        created_at=datetime.now(),
-        updated_at=datetime.now(),
+    await state.update_data(moderation_enabled=True)
+    await state.set_state(Admin.end_add_channel)
+    await main_message.message.edit_text(
+        text="Введите ID канала с комментариями или перешлите сообщение из него:",
     )
-    await add_channel(db_session, new_channel)
+
+
+@router.callback_query(Admin.change_moderation, F.data == Buttons.no_god_no_callback)
+async def add_channel_stage_5_no(
+    callback_query: types.CallbackQuery, state: FSMContext, db_session: AsyncSession
+):
+    await state.update_data(moderation_enabled=False)
+    data = await state.get_data()
+    main_message = data.get("main_message")
+    new_channel = await add_new_channel(data, db_session)
     details = get_channel_details_text(new_channel)
     await main_message.message.edit_text(
-        text=f"Канал успешно добавлен!\n\n{details}", reply_markup=go_to_main_menu_keyboard()
+        text=f"Канал успешно добавлен!\n\n{details}",
+        reply_markup=go_to_main_menu_keyboard(),
+    )
+
+
+@router.message(Admin.end_add_channel)
+async def add_channel_stage_6(
+    message: types.Message, state: FSMContext, db_session: AsyncSession
+):
+    data = await state.get_data()
+    main_message = data.get("main_message")
+    if message.forward_from_chat:
+        if message.forward_from_chat.type != "channel":
+            await message.delete()
+            await main_message.message.edit_text(
+                "❌Пересланное сообщение не является каналом"
+            )
+            return
+        else:
+            await message.delete()
+            comment_chat_id = message.forward_from_chat.id
+    else:
+        try:
+            comment_chat_id = int(message.text)
+            await message.delete()
+        except ValueError:
+            await message.delete()
+            await main_message.message.edit_text("❌Введите корректный ID канала:")
+            return
+    await state.update_data(comment_chat_id=comment_chat_id)
+    new_channel = await add_new_channel(data, db_session)
+    details = get_channel_details_text(new_channel)
+    await main_message.message.edit_text(
+        text=f"Канал успешно добавлен!\n\n{details}",
+        reply_markup=go_to_main_menu_keyboard(),
     )
