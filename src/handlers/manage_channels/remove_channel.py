0ddb99263b67ca593751aa2be73f321e17a3d33e
thevery-nvg <129183313+thevery-nvg@users.noreply.github.com>
2025-09-18 18:16:33 +0400

    total rebuild for db

--- src/handlers/manage_channels/remove_channel.py
+++ src/handlers/manage_channels/remove_channel.py
@@ -1,14 +1,12 @@
 from aiogram import Router, F, types
-from aiogram import Router, F, types
 from aiogram.fsm.context import FSMContext
-from aiogram.utils.keyboard import InlineKeyboardBuilder
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.core.crud import delete_channel
 from src.handlers.utils import (
     Buttons,
-    goto_main_menu_btn,
     Admin,
+    go_to_main_menu_keyboard,
 )
 
 router = Router(name="remove_channel")
@@ -20,33 +18,41 @@ async def remove_channel_stage_1(state: FSMContext):
     main_message = data.get("main_message")
     await state.set_state(Admin.remove_channel)
     await main_message.message.edit_text(
-        "Введите ID канала:",
+        text="Введите ID канала или перешлите сообщение из него:",
     )
-
 
 @router.message(Admin.remove_channel)
 async def remove_channel_stage_2(message: types.Message, state: FSMContext, db_session: AsyncSession):
     data = await state.get_data()
     main_message = data.get("main_message")
-    try:
-        channel_id = int(message.text)
-        await message.delete()
-    except ValueError:
-        await message.delete()
-        await main_message.message.edit_text("❌Введите корректный ID канала:")
-        return
+    if message.forward_from_chat:
+        if message.forward_from_chat.type != "channel":
+            await message.delete()
+            await main_message.message.edit_text(
+                "❌Пересланное сообщение не является каналом"
+            )
+            return
+        else:
+            await message.delete()
+            channel_id = message.forward_from_chat.id
+    else:
+        try:
+            channel_id = int(message.text)
+            await message.delete()
+        except ValueError:
+            await message.delete()
+            await main_message.message.edit_text("❌Введите корректный ID канала:")
+            return
     channels = data.get("channels")
     channel=None
     filtered_channels = []
-    for ch in channels:
-        if ch.id == channel_id:
-            channel = ch
+    for c in channels:
+        if c.id == channel_id:
+            channel = c
         else:
-            filtered_channels.append(ch)
+            filtered_channels.append(c)
     await state.update_data(channels=filtered_channels)
     await delete_channel(db_session, channel)
-    builder = InlineKeyboardBuilder()
-    builder.button(**goto_main_menu_btn)
     await main_message.message.edit_text(
-        text="Канал успешно удален!", reply_markup=builder.as_markup()
+        text="Канал успешно удален!", reply_markup=go_to_main_menu_keyboard()
     )
